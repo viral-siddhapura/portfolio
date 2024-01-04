80baa844590239182c867d0ca4de7be9a0dafa68
HVMS <viralsid2330@gmail.com>
2024-01-04 14:46:51 -0400

    Add projects page component

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Projects() {
+    return (
+        <main className='flex min-h-screen flex-col'>
+        {
+            <h1>Projects Section</h1>
+        }
+        </main>
+    );    
+}
