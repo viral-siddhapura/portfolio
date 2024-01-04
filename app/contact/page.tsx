1c401fd3320cf522c1be76ddf7a1aa7f9bf4f499
HVMS <viralsid2330@gmail.com>
2024-01-04 14:46:20 -0400

    contact us section initiated

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Contact() {
+    return (
+        <main className='flex min-h-screen flex-col'>
+        {
+            <h1>Contact Section</h1>
+        }
+        </main>
+    );    
+}
