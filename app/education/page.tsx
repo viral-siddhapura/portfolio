d9829f4eb1e34f4bfd39cf762f292ce504770a1a
HVMS <viralsid2330@gmail.com>
2024-01-04 14:46:39 -0400

    Add Education section component

---
+++ app/education/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Education() {
+    return (
+        <main className='flex min-h-screen flex-col'>
+        {
+            <h1>Education Section</h1>
+        }
+        </main>
+    );    
+}
