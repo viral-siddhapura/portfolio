904e3510d7a15b39fa7c12336e2dc211db174d06
HVMS <viralsid2330@gmail.com>
2024-01-04 14:47:11 -0400

    Add Experience section component

---
+++ app/experience/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Experience() {
+    return (
+        <main className='flex min-h-screen flex-col'>
+        {
+            <h1>Experience Section</h1>
+        }
+        </main>
+    );    
+}
