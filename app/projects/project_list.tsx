88da50669af8d9222325c26dc43c8fd7bb0a3751
HVMS <viralsid2330@gmail.com>
2024-01-07 13:30:54 -0400

    Animation added and fixed stroke elements into svg path

--- app/projects/project_list.tsx
+++ app/projects/project_list.tsx
@@ -22,7 +22,7 @@ export default function ProjectList() {
                             <a href="https://github.com/HVMS/ASDCProject" target="_blank" className="project_link">
                                 GitHub.com
                                 <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
                                 </svg>
                             </a>
                         </div>
@@ -42,7 +42,7 @@ export default function ProjectList() {
                             <a href="https://github.com/HVMS/CloudProject" target="_blank" className="project_link">
                                 GitHub.com
                                 <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
                                 </svg>
                             </a>
                         </div>
@@ -62,7 +62,7 @@ export default function ProjectList() {
                             <a href="https://classmate-g7.netlify.app/" target="_blank" className="project_link">
                                 Netlify.com
                                 <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
                                 </svg>
                             </a>
                         </div>
@@ -82,32 +82,53 @@ export default function ProjectList() {
                             <a href="https://github.com/HVMS/Inquiry_Management" target="_blank" className="project_link">
                                 GitHub.com
                                 <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
                                 </svg>
                             </a>
                         </div>
                     </div>
-                    <div className="col-start-2 row-start-2">
-                        <div className="project_card">
-                            <Image
-                                src="/images/trivia_game.svg"
-                                alt="trivia games"
-                                width={64}
-                                height={64}
-                            />
-                            <a href="https://github.com/HVMS/Multi_Trivia_Game">
-                                <h5 className="project_title">Trivia Titans</h5>
-                            </a>
-                            <p className="project_description">A Multi-Cloud Online Gaming platform designed for collaborative fun learning.</p>
-                            <a href="https://github.com/HVMS/Multi_Trivia_Game" target="_blank" className="project_link">
-                                GitHub.com
-                                <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
-                                </svg>
-                            </a>
+                        <div className="col-start-2 row-start-2">
+                            <div className="project_card">
+                                <Image
+                                    src="/images/trivia_game.svg"
+                                    alt="trivia games"
+                                    width={64}
+                                    height={64}
+                                />
+                                <a href="https://github.com/HVMS/Multi_Trivia_Game">
+                                    <h5 className="project_title">Trivia Titans</h5>
+                                </a>
+                                <p className="project_description">A Multi-Cloud Online Gaming platform designed for collaborative fun learning.</p>
+                                <a href="https://github.com/HVMS/Multi_Trivia_Game" target="_blank" className="project_link">
+                                    GitHub.com
+                                    <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    <div className="col-start-3 row-start-2">
+                        <div className="col-start-2 row-start-2">
+                            <div className="project_card">
+                                <Image
+                                    src="/images/phamra_trac.svg"
+                                    alt="Pharma trac application"
+                                    width={64}
+                                    height={64}
+                                />
+                                <a href="https://github.com/HVMS/pharma_trac">
+                                    <h5 className="project_title">Pharma Trac</h5>
+                                </a>
+                                <p className="project_description">Flutter based Health care application to measure vital signs and ADRs of drugs.</p>
+                                <a href="https://github.com/HVMS/pharma_trac" target="_blank" className="project_link">
+                                    GitHub.com
+                                    <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+                                    </svg>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                    <div className="col-start-3 row-start-2">8</div>
                 </div>
             </div>
             <div className="basis-1/6"></div>
