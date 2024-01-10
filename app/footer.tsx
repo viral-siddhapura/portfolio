63d75529479fe029905b0adb08335384aa4d13b8
HVMS <viralsid2330@gmail.com>
2024-01-10 00:06:16 -0400

    Refactor footer component

--- app/footer.tsx
+++ app/footer.tsx
@@ -2,28 +2,15 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer>
-            <div className="w-full mx-auto max-w-screen-xl md:flex md:items-center md:justify-between">
-                <span className="text-normal text-white sm:text-center">© 2024 <Link href="#">Viral Siddhapura</Link>. All Rights Reserved.
-                </span>
-                <ul className="flex flex-wrap items-center mt-3 text-normal font-medium text-white sm:mt-0">
-                    <li>
-                        <Link href="#" className="footer_text">About</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className="footer_text">Experience</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className="footer_text">Projects</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className="footer_text">Education</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className="footer_text">Contact</Link>
-                    </li>
-                </ul>
-            </div>
+        <footer className="flex flex-col space-y-10 justify-center m-10">
+        <p className="text-normal text-white sm:text-center">&copy; 2024 Viral Siddhapura. All rights reservered.</p>
+            <nav className="flex justify-center flex-wrap gap-6 text-white font-medium">
+                <a className="footer_text" href="#">About</a>
+                <a className="footer_text" href="#">Experience</a>
+                <a className="footer_text" href="#">Projects</a>
+                <a className="footer_text" href="#">Education</a>
+                <a className="footer_text" href="#">Contact</a>
+            </nav>
         </footer>
     )
 }
