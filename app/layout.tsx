be8c11face38e75520906981ad788c73e86ac361
HVMS <viralsid2330@gmail.com>
2024-01-12 16:10:29 -0400

    Vercel package for Analytics library - added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
