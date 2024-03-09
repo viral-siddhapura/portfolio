b5872b01b4191a57b5887225022fc6252a0bdf06
HVMS <viralsid2330@gmail.com>
2024-03-09 00:55:21 -0400

    bank of america div added

--- app/experience/bank_of_america.tsx
+++ app/experience/bank_of_america.tsx
@@ -1,19 +1,60 @@
-export default function BankOfAmerica() {
+import Image from 'next/image'
+
+export default function PersuasiveComputingLab() {
     return (
-        <>
-            <div className="experience_cardview">
-                <div className="flex flex-row">
-                    <div className="experience_first_content">
-                        <p className="font-semibold text-base text-amber-300">Bank of America Corporation</p>
-                    </div>
-                    <div className="experience_second_content">
-                        <p>July 2020 - Sep 2022</p>
-                    </div>
-                </div>
-                <div className="experience_first_content">
-                    <p>Software Development Engineer</p>
-                </div>
+        <div className='p-2'>
+            <div className="flex items-center justify-between px-4 m-2">
+                <p className="text-emerald-500 text-xl font-serif font-semibold">Bank of America Corporation</p>
+                <p className="text-white font-sans font-semibold">July 2020 - Sep 2022</p>
             </div>
-        </>
+            <div className="flex items-center justify-between px-4 m-2 text-white font-sans font-semibold">
+                <p>Software Development Engineer - (Remote)</p>
+                <p>GIFT CITY, India</p>
+            </div>
+            <div className='flex flex-row px-2 mx-2 mt-10'>
+                <div className='flex flex-row justify-between'>
+                    <Image
+                        src='/images/dot.svg'
+                        width={25}
+                        height={25}
+                        alt={'education image'}
+                    />
+                </div>
+                <p className='text-white font-sans font-normal p-2'>Developed healthcare web app for vitals signs and drug reactions</p>
+            </div>
+            <div className='flex flex-row px-2 mx-2'>
+                <div className='flex flex-row justify-between'>
+                    <Image
+                        src='/images/dot.svg'
+                        width={25}
+                        height={25}
+                        alt={'education image'}
+                    />
+                </div>
+                <p className='text-white font-sans font-normal p-2'>Enhanced Authentication and Authorization with OAuth2 concepts</p>
+            </div>
+            <div className='flex flex-row px-2 mx-2'>
+                <div className='flex flex-row justify-between'>
+                    <Image
+                        src='/images/dot.svg'
+                        width={25}
+                        height={25}
+                        alt={'education image'}
+                    />
+                </div>
+                <p className='text-white font-sans font-normal p-2'>Reduced page loading time using microservices based REST APIs</p>
+            </div>
+            <div className='flex flex-row px-2 mx-2'>
+                <div className='flex flex-row justify-between'>
+                    <Image
+                        src='/images/dot.svg'
+                        width={25}
+                        height={25}
+                        alt={'education image'}
+                    />
+                </div>
+                <p className='text-white font-sans font-normal p-2'>Designed interactive app UI with vital sign graphs for visualization</p>
+            </div>
+        </div>
     )
 }
