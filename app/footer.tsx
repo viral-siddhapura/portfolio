aec707a50950d199ea3a1d8c59573e584813d491
HVMS <viralsid2330@gmail.com>
2024-03-11 23:42:27 -0300

    footer section updated

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,33 +1,21 @@
-// import Link from "next/link";
-
-// export default function Footer() {
-//     return (
-//         <footer className="flex flex-col space-y-10 justify-center m-10">
-//         <p className="text-normal text-white sm:text-center">&copy; 2024 Viral Siddhapura. All rights reservered.</p>
-//             <nav className="flex justify-center flex-wrap gap-6 text-white font-medium">
-//                 <a className="footer_text" href="#">About</a>
-//                 <a className="footer_text" href="#">Experience</a>
-//                 <a className="footer_text" href="#">Projects</a>
-//                 <a className="footer_text" href="#">Education</a>
-//                 <a className="footer_text" href="#">Contact</a>
-//             </nav>
-//         </footer>
-//     )
-// }
-
-import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col sm:flex-col items-center justify-center space-y-10 sm:space-y-10 sm:space-x-10 m-10">
-            <p className="text-normal text-white sm:text-center">&copy; 2024 Viral Siddhapura. All rights reserved.</p>
-            <nav className="flex justify-center flex-wrap gap-6 text-white font-medium">
-                <Link href="#" className="footer_text">About</Link>
-                <Link href={'/experience'} className="footer_text">Experience</Link>
-                <Link href={'/projects'} className="footer_text">Projects</Link>
-                <Link href={'/education'} className="footer_text">Education</Link>
-                <Link href={'/contact'} className="footer_text">Contact</Link>
-            </nav>
+        <footer className="flex flex-col sm:flex-col items-center justify-center m-10">
+            <div className="flex flex-row justify-center items-center">
+                <p className="text-normal text-white sm:text-center">Made by</p>
+                <div className="mx-1"></div>
+                <Image
+                    className="mx-1"
+                    src="/images/heart.svg"
+                    alt="Viral Siddhapura"
+                    width={24}
+                    height={24}
+                >
+                </Image>
+                <p className="text-normal text-white sm:text-center mx-1">Viral Siddhapura</p>
+            </div>
         </footer>
     )
 }
